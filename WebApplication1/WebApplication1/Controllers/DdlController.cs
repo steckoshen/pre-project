7b81820b40f68a96c4c6ed7caf3874438bac7c93
stecko <stecko.shen@gmail.com>
2015-07-16 15:24:20 +0800

    [Project] Add new project to test Paged GridView

---
+++ WebApplication1/WebApplication1/Controllers/DdlController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DdlController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        // GET: Ddl
+        public ActionResult Index()
+        {
+
+            ViewBag.Message = "DropDownList testing";
+            SelectList selectlist = new SelectList(this.GetCustomers(), "CustomerID", "ContactName");
+            ViewBag.Selectlist = selectlist;
+            //return View(db.Products.ToList());
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Orders(string customerID)
+        {
+
+            List<KeyValuePair<int, string>> items = new List<KeyValuePair<int, string>>();
+
+            if (!string.IsNullOrEmpty(customerID))
+            {
+                var orders = GetOrders(customerID);
+
+                foreach (var order in orders)
+                {
+                    items.Add(new KeyValuePair<int, string>(int.Parse(order.OrderID.ToString()),
+                        string.Format("{0} ({1:yyyy-MM-dd})", order.OrderID, order.OrderDate)));
+                }
+            }
+            JsonResult j = this.Json(items);
+            return j;
+        }
+
+        [HttpPost]
+        public JsonResult Products(string orderID) 
+        {
+            List<KeyValuePair<string, string>> items = new List<KeyValuePair<string, string>>();
+
+            int id =0;
+
+            if (!string.IsNullOrEmpty(orderID) && int.TryParse(orderID, out id)) 
+            {
+                    var products = GetProducts(id);
+                    if (products.Count() > 0)
+                    {
+                        foreach (var product in products)
+                        {
+                            items.Add(new KeyValuePair<string, string>(
+                                product.ProductID.ToString(), product.ProductName));
+                        }
+                    }
+                
+            }
+
+            return this.Json(items);
+        }
+
+        public ActionResult ProductInfo(string productID) 
+        {
+            int id = 0;
+            if (!string.IsNullOrEmpty(productID) && int.TryParse(productID, out id)) {
+                var productinfo = GetProductInfo(id);
+
+                ViewData.Model = productinfo;
+            }
+            return PartialView("_ProductInfoPartialView");
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            var query = db.Customers.OrderBy(x => x.CustomerID);
+            return query.ToList();
+        }
+
+        private IEnumerable<Order> GetOrders(string customerID)
+        {
+            var query = db.Orders.Where(x => x.CustomerID == customerID).OrderBy(x => x.OrderDate);
+            return query.ToList();
+        }
+
+        private IEnumerable<Product> GetProducts(int orderID)
+        {
+            var query = db.Order_Details.Where(x => x.OrderID == orderID).Select(x => x.Product);
+            return query.ToList();
+        }
+
+        private Product GetProductInfo(int productID) {
+            
+            return db.Products.FirstOrDefault(x => x.ProductID == productID);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/TablePagingController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Linq.Dynamic;
+using System.Data.Entity.Core.Objects.DataClasses;
+using System.Reflection;
+using MvcPaging;
+
+namespace WebApplication1.Controllers
+{
+    public class TablePagingController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+        string FilterCity, SortColumnName, SortType;
+        private const int DefaultPageSize = 10;
+        private int page = 0;
+
+        // GET: Ddl
+        public ActionResult Index(int page=0)
+        {
+            PrepareDropDownList();
+
+            ViewData.Model = new List<Customer>();
+            
+            ViewBag.FilterCity = this.FilterCity;
+            ViewBag.SortColumnName = this.SortColumnName;
+            ViewBag.SortType = this.SortType;
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(string city = "all", string columnName="CompanyName", string sort = "asc")
+        {
+            ViewBag.Message = "Paging Gridview";
+
+            PrepareDropDownList();
+            var query = db.Customers.Select(x => x);
+
+            if (!city.Equals("all")) {
+                 query = query.Where("City == @0", city ?? "Lodon");
+            }
+
+            query = query.OrderBy(string.Format("{0} {1}", columnName, sort));
+            ViewData.Model = query.ToList();
+
+            return View();
+        }
+
+        private void PrepareDropDownList() {
+
+            GetCityDDL();
+            GetColumnNameDDL();
+            GetSortingRuleDDL();
+        }
+
+        /// <summary>
+        /// Prepare for DropDownList: City
+        /// </summary>
+        private void GetCityDDL() {
+
+            SelectList selectlist = new SelectList(this.GetCityList(), "Value", "Text");
+            ViewBag.CityList = selectlist;
+
+        }
+
+        private void GetColumnNameDDL() {
+            
+            SelectList selectlist = new SelectList(this.GetColumnNameList(), "Value", "Text");
+            ViewBag.ColumnNameList = selectlist;
+
+        }
+
+        private void GetSortingRuleDDL() {
+            List<SelectListItem> sortList = new List<SelectListItem>();
+            sortList.Add(new SelectListItem() { Text = "ASC", Value = "asc" });
+            sortList.Add(new SelectListItem() { Text = "DESC", Value = "desc" });
+            SelectList selectlist = new SelectList(sortList, "Value", "Text");
+            ViewBag.SortList = selectlist;
+        }
+
+        /// <summary>
+        /// Get CityList in Customers
+        /// </summary>
+        /// <returns></returns>
+        private List<SelectListItem> GetCityList()
+        {
+            var cities = db.Customers.Select(x => x.City).OrderBy(x => x).Distinct();
+
+            List<SelectListItem> cityList = new List<SelectListItem>();
+            cityList.Add(new SelectListItem() { Text = "All City", Value = "all" });
+            foreach (var city in cities) {
+                cityList.Add(new SelectListItem() { Value = city, Text = city });
+            }
+
+            return cityList;
+        }
+
+        /// <summary>
+        /// Get ColumnName List by LingObj
+        /// </summary>
+        /// <returns></returns>
+        private List<SelectListItem> GetColumnNameList() {
+
+            var linqObj = new Customer();
+
+            List<SelectListItem> columnNameList = new List<SelectListItem>();
+            foreach (PropertyInfo pI in linqObj.GetType().GetProperties().Distinct())
+            {
+                columnNameList.Add(new SelectListItem() { Value = pI.Name, Text = pI.Name });
+            }
+
+            return columnNameList;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
